9511f084853bac0b89c7e5febfead7ad44733fde
Erin Kelkar <ekelkar@gmail.com>
2017-11-03 11:07:10 -0400

    Set up large video viewer at top of screen that adjusts if window is resized.

--- myyoutube.js
+++ myyoutube.js
@@ -11,23 +11,24 @@ $(document).ready(function () {
       // let videoId = video.id.videoId;  // old way
       let videoId = $(event.currentTarget).attr('data-video-id');
       videoEmbedElement.attr('src', youtube.generateEmbedUrl(videoId));
-      videoEmbedElement.attr('width', 560);
-      videoEmbedElement.attr('height', 318);
+      // videoEmbedElement.attr('width', 560);
+      // videoEmbedElement.attr('height', 315);
+      videoEmbedElement.attr('class', 'embed-responsive-item');
       return videoEmbedElement;
     };
 
     event.preventDefault();
-    let videoHead = $('<h2>');
-    // let videoTitle = video.snippet.title;
-    let videoTitle = $(event.currentTarget).attr('data-video-title');
-    // let videoId = $(event.currentTarget).attr('data-video-id');
-    videoHead.html(videoTitle);
+    // let videoHead = $('<h2>');
+    // // let videoTitle = video.snippet.title;
+    // let videoTitle = $(event.currentTarget).attr('data-video-title');
+    // // let videoId = $(event.currentTarget).attr('data-video-id');
+    // videoHead.html(videoTitle);
 
     var videoWatcher = $('#video-watcher');
     // videoWatcher.hide();  // hide the matched elements
     // remove all child nodes of the set of matched elements from the DOM
     videoWatcher.empty();
-    videoWatcher.append(videoTitle);
+    // videoWatcher.append(videoTitle);
     videoWatcher.append(createVideoEmbedElement());
     videoWatcher.show();
     // videoWatcher.fadeIn(); // display the matched elements by fading them to opaque
@@ -53,13 +54,6 @@ $(document).ready(function () {
   function addVideoToList(video) {
     console.log(video);
 
-    // var thumbnailUrl2 = youtube.generateThumbnailUrl(video.id.videoId);
-    // var thumbnailImg2 = $('<img>')
-    // thumbnailImg2.attr('src', video.snippet.thumbnails.default.url);
-
-    // var tnailImg = $('<img>');
-    // tnailImg.attr('src', video.snippet.thumbnails.default.url);
-
     // Create a div to put info into
     let videoDiv = createVideoDiv(video);
 
@@ -76,70 +70,9 @@ $(document).ready(function () {
     // Add these to the videos-list element
     $('#videos-list').append(videoListItem);
 
-    //     var videoDiv2 = $('<div>');
-    //     videoDiv2.attr('class', 'thumbnail')
-    //     videoDiv2.append(thumbnailImg);
-    //     console.log('videoDiv2:', videoDiv2);
-    //     console.log('videoDiv: ', videoDiv);
-    //     console.log('tni2: ', thumbnailImg)
-    //     var card = `
-    //   <div class="thumbnail">
-    //     <img src="${video.snippet.thumbnails.default.url}"/> 
-    //     <div class="caption" style="overflow: hidden;">
-    //       <h4>${video.snippet.title}</h4>
-    //       <p>${video.snippet.description} </p>
-    //     </div>
-    //   </div>`
-    //     console.log('card: ', card)
-
-    // var captionDiv = $('<div>');
-    // captionDiv.attr('class', 'caption');
-    // captionDiv.append($('<h4>' + video.snippet.title + '</h4>'));
-    // captionDiv.append($('<p>' + video.snippet.description + '</p>'));
-    // videoDiv2.append(captionDiv);
-
     videoDiv.on('click', handleVideoClick);
-    // function (event) {
-    //   function createVideoEmbedElement (video) {
-    //     let videoEmbedElement = $('<iframe></iframe>');
-    //     // let videoId = video.id.videoId;  // old way
-    //     let videoId = $(event.currentTarget).attr('data-video-id');
-    //     videoEmbedElement.attr('src', youtube.generateEmbedUrl(videoId));
-    //     videoEmbedElement.attr('width', 560);
-    //     videoEmbedElement.attr('height', 318);
-    //     return videoEmbedElement
-    //   }
-
-    //   event.preventDefault();
-    //   let videoHead = $('<h2>');
-    //   // let videoTitle = video.snippet.title;
-    //   let videoTitle = $(event.currentTarget).attr('data-video-title');
-    //   videoHead.html(videoTitle);
-
-    //   var videoWatcher = $('#video-watcher');
-    //   // videoWatcher.hide();  // hide the matched elements
-    //   // remove all child nodes of the set of matched elements from the DOM
-    //   videoWatcher.empty();
-    //   videoWatcher.append(videoTitle);
-    //   videoWatcher.append(createVideoEmbedElement(video));
-    //   videoWatcher.show();
-    //   // videoWatcher.fadeIn(); // display the matched elements by fading them to opaque
-    // });
-
-    // var videoItem = $('<li>');
-    // console.log('videoDiv: ', videoDiv);
-    // videoItem.append(videoDiv);
-    // $('#videos-list').append(videoItem);
-
-    // var videoItem2 = $('<li>');
-    // videoItem2.append(videoDiv2);
-    // $('#videos-list2').append(videoItem2);
-
-    // var videoItem3 = $('<li>');
-    // videoItem3.append(card);
-    // $('#videos-list3').append(videoItem3);
-  }
-
+  };
+  
   function addVideoToList2(video) {
     // Add the same video but change the div to have class thumbnail
     console.log(video);
